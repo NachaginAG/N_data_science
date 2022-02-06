e9925d68eb3146d6766ef4e60da0f11b1eb9c030
NachaginAG <nachagin@mail.ru>
2022-02-06 16:59:10 +0400

    First commit

---
+++ game.py
@@ -0,0 +1,14 @@
+from itertools import count
+import numpy as np
+number = np.random.randint(1,101) #загадываем число
+count = 0
+while True:
+    count += 1
+    predict_number = int(input("Угадай сисло от 1 до 100  :"))
+    if predict_number > number:
+        print("Число должно быть меньше")
+    elif predict_number < number:
+        print("Число должно быть больше")
+    else:
+        print(f"Вы угадали число! Это число = {number}, за {count} попыток")
+        break # конц игры, выход из цикла
